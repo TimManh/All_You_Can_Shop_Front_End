1ed0ffc99f22f8a74f8a1ead0f9f7485034769de
bob <timmanh93@gmail.com>
2020-09-01 20:08:15 -0700

    Second component in Home

--- Components/Main/Shop/Home/Category.js
+++ Components/Main/Shop/Home/Category.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
-import bannerImage from "../../../../Resources/images-master/temp/banner.jpg";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import Swiper from "react-native-swiper";
+import littleIcom from "../../../../Resources/images-master/temp/little.jpg";
+import maxiIcon from "../../../../Resources/images-master/temp/maxi.jpg";
+import partyIcon from "../../../../Resources/images-master/temp/party.jpg";
+
 const { width, height } = Dimensions.get("window");
 
 export default function Category() {
   return (
     <View style={styles.wrapper}>
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text style={styles.textStyle}>SPRING COLLECTION</Text>
+        <Text style={styles.textStyle}>LIST OF CATEGORY</Text>
       </View>
       <View style={{ flex: 4 }}>
-        <Image source={bannerImage} style={styles.imageStyle} />
+        <Swiper>
+          <ImageBackground source={littleIcom} style={styles.imageStyle}>
+            <Text style={styles.cateTitle}>Little Dress</Text>
+          </ImageBackground>
+          <ImageBackground source={maxiIcon} style={styles.imageStyle}>
+            <Text style={styles.cateTitle}>Maxi Dress</Text>
+          </ImageBackground>
+          <ImageBackground source={partyIcon} style={styles.imageStyle}>
+            <Text style={styles.cateTitle}>Party Dress</Text>
+          </ImageBackground>
+        </Swiper>
       </View>
     </View>
   );
@@ -21,6 +41,7 @@ const styles = StyleSheet.create({
   wrapper: {
     height: height * 0.32,
     backgroundColor: "#fff",
+    justifyContent: "space-between",
     margin: 10,
     shadowColor: "#2E272B",
     shadowOffset: { width: 0, height: 3 },
@@ -35,5 +56,11 @@ const styles = StyleSheet.create({
   imageStyle: {
     height: imageHeight,
     width: imageWidth,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cateTitle: {
+    fontSize: 20,
+    color: "#9A9A9A",
   },
 });
