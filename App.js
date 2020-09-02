131031b320138bf85fcc589058c15911134c468d
bob <timmanh93@gmail.com>
2020-09-02 16:25:05 -0700

    Modify Drawer Screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Main from "./Components/Main/Main";
-
+import DrawerContent from "./Components/Main/DrawerContent/DrawerContent";
 import Authentication from "./Components/Authentication/Authentication";
 import ChangeInfo from "./Components/ChangeInfo/ChangeInfo";
 import OrderHistory from "./Components/OrderHistory/OrderHistory";
@@ -11,7 +11,7 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={Main} />
         <Drawer.Screen name="Authentication" component={Authentication} />
         <Drawer.Screen name="Change Info" component={ChangeInfo} />
